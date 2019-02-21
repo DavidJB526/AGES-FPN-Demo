6089b0f14009ee423c84cd9364b235416a473797
David Beaudoin <David.Beaudoin@iam.local>
2019-02-21 17:19:26 -0600

    Add First Person Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveVertical, 0, moveHorizontal);
+
+        rb.AddForce(movement, ForceMode.Impulse);
+    }
+}
