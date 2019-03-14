0d87a7f874bee4bfb16b2fa3b1130d6cc50540b5
David Beaudoin <David.Beaudoin@iam.local>
2019-03-14 18:16:41 -0500

    Add DetectInteractiveObject Script, Create TestScene

---
+++ Assets/Scripts/DetectInteractiveObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInteractiveObjects : MonoBehaviour
+{
+    [SerializeField]
+    private Transform raycastOrigin;
+    [SerializeField]
+    private float maxDetectRange = 5.0f;
+    [SerializeField]
+    private int layerToIgnore = 9;
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxDetectRange, Color.green);
+
+        RaycastHit hitInfo;
+        bool objectDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxDetectRange);
+
+        if (objectDetected)
+        {
+            Debug.Log(hitInfo.collider.gameObject.name);
+        }
+    }
+}
