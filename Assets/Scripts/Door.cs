c6659adde7f9400a114dec11d6a38f5c6c139e26
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-03-28 15:24:48 -0500

    Add Doors and Door Animations

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : InteractiveObject
+{
+    private Animator anim;
+    private int shouldOpenAnimParameter = Animator.StringToHash("shouldOpen");
+
+    /// <summary>
+    /// Using a contructor here to initialize displayText in the Editor
+    /// </summary>
+    public Door()
+    {
+        displayText = nameof(Door);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        anim = GetComponent<Animator>();
+    }
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        anim.SetBool(shouldOpenAnimParameter, !anim.GetBool(shouldOpenAnimParameter));
+    }
+}
