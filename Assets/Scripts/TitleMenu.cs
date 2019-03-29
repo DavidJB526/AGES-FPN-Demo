af6090a0d11bd52fce1fd9f6989bbcac6e4be649
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-03-29 13:18:38 -0500

    Add TextMeshPro Assets and Basic TitleMenu

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void LoadGameScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
