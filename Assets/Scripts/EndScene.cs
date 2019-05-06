74013137f587753d9b70279fd1013b210384bde8
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-05-05 21:31:02 -0500

    Add Assets, Full Game Loop, Bake Lighting

---
+++ Assets/Scripts/EndScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider))]
+public class EndScene : MonoBehaviour
+{
+    [SerializeField]
+    private Image blackScreenPanel;
+    [SerializeField]
+    private string sceneToLoad;
+    [SerializeField]
+    private float timeToFade;
+
+    private void Awake()
+    {
+        blackScreenPanel.CrossFadeAlpha(0, 0, true);
+    }
+
+    private void Start()
+    {
+        this.gameObject.SetActive(false);        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            blackScreenPanel.CrossFadeAlpha(1, timeToFade, true);
+            StartCoroutine(TriggerEnd());
+        }
+    }
+
+    IEnumerator TriggerEnd()
+    {
+        yield return new WaitForSeconds(timeToFade + .5f);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
